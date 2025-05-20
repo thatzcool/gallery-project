ce82cf0acabc9f6b8ae34fc99cad80066609c7fc
thatzcool <mycaptain0771@gmail.com>
2025-05-20 16:04:25 +0900

    gallery-cart_adding_01

---
+++ src/main/java/com/ssg/gallery/cart/dto/CartRead.java
@@ -0,0 +1,12 @@
+package com.ssg.gallery.cart.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class CartRead {
+
+    private Integer id;
+    private Integer itemId;
+}
