b14e230d3907d777d7c95bc254741af9da1de72e
thatzcool <mycaptain0771@gmail.com>
2025-05-21 12:50:24 +0900

    gallery-forntend-implement_03

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -13,36 +13,35 @@ const accountStore = useAccountStore();
 const route = useRoute();
 
 // 로그인 여부를 확인하는 체크 메서드 , 계정서비스의 check메서드를 호출해서 로그인 여부를 확인 후, 해당 값으로 계정 스토어 객체를 수정한다.
- const checkAccount = async () => {
-         const res = await check();
+const checkAccount = async () => {
+  const res = await check();
 
-         if(res.status === 200){
-           accountStore.setChecked(true);
-           accountStore.setLoggedIn(res.data === true);
-         } else {
-           accountStore.setChecked(false);
-         }
- }
+  if (res.status === 200) {
+    accountStore.setChecked(true);
+    accountStore.setLoggedIn(res.data === true);
+  } else {
+    accountStore.setChecked(false);
+  }
+};
 
- // 커스텀 생성 훅
-(async function onCreated(){
-       await checkAccount();
-});
+// 커스텀 생성 훅
+(async function onCreated() {
+  await checkAccount();
+})();
 // 라우트 경로가 바뀔 때마다 로그인 여부를 확인
-watch(() => route.path, ()=> {
-       checkAccount();
+watch(() => route.path, () => {
+  checkAccount();
 });
 
 </script>
 <template>
   <!-- 로그인 체크 여부 확인 후 출력-->
-
   <template v-if="accountStore.checked">
-    <Header />
-  <main>
-<!-- 라우터 뷰 url 경로에 따라 동적으로 컨텐츠가 바뀔 수 있는 요소를 배치한다.-->
+    <Header/>
+    <main>
+      <!-- 라우터 뷰 url 경로에 따라 동적으로 컨텐츠가 바뀔 수 있는 요소를 배치한다.-->
       <router-view></router-view>
-  </main>
-  <Footer />
+    </main>
+    <Footer/>
   </template>
- </template>
+</template>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   server:{
+
+      proxy: {
+            "/v1/api" : {
+                 target: "http://localhost:8080",
+                 changeOrigin: true
+            }
+      }
+
+   },
+
+
   plugins: [
     vue(),
   ],
@@ -14,3 +25,9 @@ export default defineConfig({
     }
   }
 })
+
+
+// 3. CORS(Cross-Origin Resouce Sharing)
+// 클라이언트와 서버의 URL  http://localhost  포트가(5173,8080) 서로 다르기 때문에
+//     CORS(Cross-Origin Resouce Sharing) 문제가 발생함.
+//     서버 객체에 proxy를 추가한다.
