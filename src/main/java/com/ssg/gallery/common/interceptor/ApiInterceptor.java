3223d2cdc62464694fcabfb1d3a3b53baf688838
thatzcool <mycaptain0771@gmail.com>
2025-05-23 15:28:09 +0900

    gallery-implement_interceptor_backend02

--- src/main/java/com/ssg/gallery/common/interceptor/ApiInterceptor.java
+++ src/main/java/com/ssg/gallery/common/interceptor/ApiInterceptor.java
@@ -1,15 +1,25 @@
 package com.ssg.gallery.common.interceptor;
 
+import com.ssg.gallery.account.helper.AccountHelper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component   //스프링컨테이너에서 관리하는 컴포넌트임을 나타냄
+@RequiredArgsConstructor
 public class ApiInterceptor implements HandlerInterceptor {
+
+    private final AccountHelper accountHelper;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //만약 로그인 회원 아이디가 없다면
+            if(accountHelper.getMemberId(request) == null) {
+                response.setStatus(401);
+                return false;
+            }
         return true;
     }
 }
