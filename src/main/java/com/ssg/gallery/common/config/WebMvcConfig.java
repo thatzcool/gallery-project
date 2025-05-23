0c443f5c51eebb25c21bd774678e94f6361e6c59
thatzcool <mycaptain0771@gmail.com>
2025-05-23 14:49:32 +0900

    gallery-implement_interceptor_backend01

---
+++ src/main/java/com/ssg/gallery/common/config/WebMvcConfig.java
@@ -0,0 +1,23 @@
+package com.ssg.gallery.common.config;
+// 어플리케이션 설정
+
+import com.ssg.gallery.common.interceptor.ApiInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+
+        private final ApiInterceptor apiInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(apiInterceptor)
+       .addPathPatterns("/v1/api/**")
+        .excludePathPatterns("/v1/api/account/**","/v1/api/items/**");   // 예외
+    }
+}
---
+++ src/main/java/com/ssg/gallery/common/interceptor/ApiInterceptor.java
@@ -0,0 +1,15 @@
+package com.ssg.gallery.common.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component   //스프링컨테이너에서 관리하는 컴포넌트임을 나타냄
+public class ApiInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+}
